bef775b89312b76dd7d5ab49ef7ce9a07e353b5f
James Jarvis <git@jamesjarvis.io>
2022-12-26 17:44:11 +0000

    add the quotes in micropython

--- micropython/examples/badger2040/micropython-builtins.cmake
+++ micropython/examples/badger2040/micropython-builtins.cmake
@@ -40,6 +40,7 @@ convert_image(usermod_badger2040 badgerpunk)
 convert_image(usermod_badger2040 launchericons)
 
 convert_raw(usermod_badger2040 289-0-wind-in-the-willows-abridged.txt witw)
+convert_raw(usermod_badger2040 quotes.txt quotes)
 
 copy_module(usermod_badger2040 ${CMAKE_CURRENT_LIST_DIR}/assets/boot.py boot)
 copy_module(usermod_badger2040 ${CMAKE_CURRENT_LIST_DIR}/launcher.py _launcher)
